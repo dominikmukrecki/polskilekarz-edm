7a3839c8f3e9de834b2deb406f41ef79ed492200
dominikmukrecki <dominik@mukrecki.pl>
2023-03-05 13:08:00 +0100

    Add hooks to calculate displayedName in Patients collection.

--- src/collections/Patients.ts
+++ src/collections/Patients.ts
@@ -1,4 +1,6 @@
-import { CollectionConfig } from 'payload/types';
+import { CollectionConfig, Hook } from 'payload/types';
+import { useState } from 'react';
+import { useHook } from '@payloadcms/config/dist/admin/useHook';
 
 const Patients: CollectionConfig = {
   slug: 'patients',
@@ -46,6 +48,25 @@ const Patients: CollectionConfig = {
       },
     },
   ],
+  hooks: {
+    beforeChange: [
+      async (hook: Hook) => {
+        const { data } = hook;
+        const { name, birthdate } = data;
+
+        if (name && birthdate) {
+          const formattedBirthdate = new Date(birthdate).toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+          });
+          data.displayedName = `${name} - ${formattedBirthdate}`;
+        }
+
+        return hook;
+      },
+    ],
+  },
 };
 
 export default Patients;
