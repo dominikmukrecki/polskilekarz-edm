254af44c282d38b1b92ffcaeea950b7aa5632b55
dominikmukrecki <dominik@mukrecki.pl>
2023-03-06 00:08:09 +0100

    Add Prescription collection with patient and medicine relations and calculated expiration date field

---
+++ src/collections/Prescriptions.ts
@@ -0,0 +1,80 @@
+import { CollectionConfig, CollectionBeforeValidateHook } from 'payload/types';
+
+interface Prescription {
+  patient: string;
+  medicine: string;
+  issuingDate: Date;
+  daysOfValidity: number;
+  expirationDate?: Date;
+}
+
+const calculateExpirationDate: CollectionBeforeValidateHook<Prescription> = async ({ data }) => {
+  const { issuingDate, daysOfValidity } = data;
+  if (!issuingDate || !daysOfValidity) return data;
+
+  const expirationDate = new Date(issuingDate.getTime() + daysOfValidity * 24 * 60 * 60 * 1000);
+  data.expirationDate = expirationDate;
+  return data;
+};
+
+const Prescription: CollectionConfig = {
+  slug: 'prescriptions',
+  fields: [
+    {
+      name: 'patient',
+      label: 'Patient',
+      type: 'relationship',
+      relationTo: 'patients',
+      required: true,
+      admin: {
+        width: '50%',
+      },
+    },
+    {
+      name: 'medicine',
+      label: 'Medicine',
+      type: 'relationship',
+      relationTo: 'medicines',
+      required: true,
+      admin: {
+        width: '50%',
+      },
+    },
+    {
+      name: 'issuingDate',
+      label: 'Date of Issuing',
+      type: 'date',
+      required: true,
+      admin: {
+        width: '50%',
+      },
+    },
+    {
+      name: 'daysOfValidity',
+      label: 'Days of Validity',
+      type: 'number',
+      required: true,
+      admin: {
+        width: '50%',
+      },
+    },
+    {
+      name: 'expirationDate',
+      label: 'Expiration Date',
+      type: 'date',
+      required: false,
+      admin: {
+        readOnly: true,
+        position: 'sidebar',
+      },
+    },
+  ],
+  admin: {
+    useAsTitle: 'medicine.commonName',
+  },
+  hooks: {
+    beforeValidate: [calculateExpirationDate],
+  },
+};
+
+export default Prescription;
