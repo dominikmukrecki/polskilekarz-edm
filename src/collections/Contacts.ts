1048f9b6e4b031d12fe1ad2873d979872875b0e9
dominikmukrecki <dominik@mukrecki.pl>
2023-03-05 21:46:22 +0100

    Refactor Patients and Contacts collections

--- src/collections/Contacts.ts
+++ src/collections/Contacts.ts
@@ -6,57 +6,71 @@ const beforeChangeHook: CollectionBeforeChangeHook = async ({ data }) => {
   return data;
 };
 
-const contactFields = [
-  {
-    name: 'name',
-    label: 'Name',
-    type: 'text',
-    required: true,
-  },
-  {
-    name: 'email',
-    label: 'Email',
-    type: 'email',
-    required: true,
-  },
-  {
-    name: 'phone',
-    label: 'Phone',
-    type: 'text',
-    required: false,
-    validate: (value) => {
-      const regex = /^\+(?:[0-9] ?){6,14}[0-9]$/;
-      if (value && !regex.test(value)) {
-        return 'Invalid phone number format';
-      }
-      return true;
-    },
-  },
-  {
-    name: 'address',
-    label: 'Address',
-    type: 'text',
-    required: true,
-  },
-  {
-    name: 'displayName',
-    label: 'Display Name',
-    type: 'text',
-    required: true,
-    defaultValue: 'New Contact',
-    admin: {
-      position: 'sidebar',
-      readOnly: true,
-    },
-  },
-];
-
 const Contacts: CollectionConfig = {
   slug: 'contacts',
   access: {
     read: () => true,
   },
-  fields: contactFields,
+  fields: [
+    {
+      type: 'row',
+      fields: [
+        {
+          name: 'name',
+          label: 'Name',
+          type: 'text',
+          required: true,
+        },
+        {
+          name: 'email',
+          label: 'Email',
+          type: 'email',
+          required: true,
+        },
+      ],
+      admin: {
+        width: '50%',
+      },
+    },
+    {
+      type: 'row',
+      fields: [
+        {
+          name: 'phone',
+          label: 'Phone',
+          type: 'text',
+          required: false,
+          validate: (value) => {
+            const regex = /^\+(?:[0-9] ?){6,14}[0-9]$/;
+            if (value && !regex.test(value)) {
+              return 'Invalid phone number format';
+            }
+            return true;
+          },
+        },
+        {
+          name: 'address',
+          label: 'Address',
+          type: 'text',
+          required: true,
+        },
+      ],
+      admin: {
+        width: '50%',
+      },
+    },
+    {
+      name: 'displayName',
+      label: 'Display Name',
+      type: 'text',
+      required: true,
+      defaultValue: 'New Contact',
+      admin: {
+        position: 'sidebar',
+        readOnly: true,
+      },
+    },
+  ],
   admin: {
     useAsTitle: 'displayName',
   },
