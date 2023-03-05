63a3f7b21d2407497aedefa69a6aa127dd32c46b
dominikmukrecki <dominik@mukrecki.pl>
2023-03-05 20:46:07 +0100

    Add displayName field to Users collection

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,6 +1,28 @@
-import { CollectionConfig } from 'payload/types';
+import { CollectionConfig, Field } from 'payload/types';
+import { useState, useEffect } from 'react';
 
-const userFields = [
+interface User {
+  name: string;
+  email: string;
+}
+
+const useDisplayName = (user: User): string => {
+  const [displayName, setDisplayName] = useState('');
+
+  useEffect(() => {
+    if (user.name && user.email) {
+      setDisplayName(`${user.name} (${user.email})`);
+    } else if (user.name) {
+      setDisplayName(user.name);
+    } else if (user.email) {
+      setDisplayName(user.email);
+    }
+  }, [user]);
+
+  return displayName;
+};
+
+const userFields: Field[] = [
   {
     name: 'name',
     label: 'Name',
@@ -38,6 +60,15 @@ const userFields = [
       position: 'sidebar',
     },
   },
+  {
+    name: 'displayName',
+    label: 'Display Name',
+    type: 'text',
+    admin: {
+      readOnly: true,
+      position: 'sidebar',
+    },
+  },
   // Add more fields as needed
 ];
 
@@ -49,6 +80,14 @@ const Users: CollectionConfig = {
   },
   access: {
     read: () => true,
+  },
+  hooks: {
+    afterChange: async ({ operation, doc }) => {
+      if (operation === 'create' || operation === 'update') {
+        const displayName = useDisplayName({ name: doc.name, email: doc.email });
+        await payload.update('users', { id: doc.id }, { displayName });
+      }
+    },
   },
   fields: userFields,
 };
