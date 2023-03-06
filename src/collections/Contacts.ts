7b456873626b0884a167a85b5736589f1d1a28de
dominikmukrecki <dominik@mukrecki.pl>
2023-03-06 22:14:38 +0100

    feat: add generateDisplayName hook to dynamically create displayName field in collection if not present

--- src/collections/Contacts.ts
+++ src/collections/Contacts.ts
@@ -1,14 +1,5 @@
-import { CollectionConfig, CollectionBeforeChangeHook } from "payload/types";
-
-const generateDisplayNameHook: CollectionBeforeChangeHook = async ({
-  data,
-}) => {
-  const { name, contactEmails, contactPhones } = data;
-  const email = contactEmails?.[0]?.email || '';
-  const phone = contactPhones?.[0]?.phone || '';
-  data.displayName = `${name}, ${email}, ${phone}`;
-  return data;
-};
+import { CollectionConfig } from "payload/types";
+import generateDisplayNameHook from '../hooks/generateDisplayNameHook';
 
 const Contacts: CollectionConfig = {
   slug: "contacts",
---
+++ src/hooks/generateDisplayNameHook.ts
@@ -0,0 +1,40 @@
+import { CollectionBeforeChangeHook, Context } from 'payload/types';
+
+type RecordData = {
+  [key: string]: any;
+};
+
+type GenerateDisplayNameArgs = {
+  defaultLabel: string;
+};
+
+const generateDisplayName = ({ defaultLabel }: GenerateDisplayNameArgs) => {
+  return async ({ data, collection }: Context<RecordData>): Promise<RecordData> => {
+    const singularLabel = collection.labels.singular;
+
+    if (!data.displayName) {
+      data.displayName = `New ${singularLabel}`;
+    }
+
+    const displayNameField = collection.fields.find((field) => field.slug === 'displayName');
+
+    if (!displayNameField) {
+      await payload.fields.add(collection, {
+        type: 'text',
+        name: 'Display Name',
+        slug: 'displayName',
+        label: 'Display Name',
+        required: false,
+        defaultValue: `New ${singularLabel}`,
+      });
+    }
+
+    return data;
+  };
+};
+
+const generateDisplayNameHook: CollectionBeforeChangeHook = generateDisplayName({
+  defaultLabel: 'New Record',
+});
+
+export default generateDisplayNameHook;
