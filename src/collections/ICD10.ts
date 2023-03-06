a32e9f78cb0f49ea08e19312bc6f8b8e36288a34
dominikmukrecki <dominik@mukrecki.pl>
2023-03-06 01:38:40 +0100

    Add translation to ICD10 collection and generateDisplayName hook

--- src/collections/ICD10.ts
+++ src/collections/ICD10.ts
@@ -1,53 +1,41 @@
-import { CollectionConfig, CollectionBeforeValidateHook } from 'payload/types';
-
-interface ICD10Data {
-  code: string;
-  description: string;
-}
-
-const generateDisplayName: CollectionBeforeValidateHook<ICD10Data> = async ({ data }) => {
-  const { code, description } = data;
-  data.displayName = `${code} - ${description}`;
-  return data;
-};
-
 const ICD10: CollectionConfig = {
-  slug: 'icd10',
-  labels: {
-    singular: 'ICD-10 Code',
-    plural: 'ICD-10 Codes',
-  },
-  fields: [
-    {
-      name: 'code',
-      label: 'Code',
-      type: 'text',
-      required: true,
-      unique: true,
+    slug: 'icd10',
+    labels: {
+      singular: 'ICD-10 Code',
+      plural: 'ICD-10 Codes',
     },
-    {
-      name: 'description',
-      label: 'Description',
-      type: 'text',
-      required: true,
-    },
-    {
-      name: 'displayName',
-      label: 'Display Name',
-      type: 'text',
-      required: true,
-      admin: {
-        position: 'sidebar',
-        readOnly: true,
+    fields: [
+      {
+        name: 'code',
+        label: 'Code',
+        type: 'text',
+        required: true,
+        unique: true,
       },
+      {
+        name: 'description',
+        label: 'Description',
+        type: 'text',
+        required: true,
+        i18n: true, // <-- add i18n property
+      },
+      {
+        name: 'displayName',
+        label: 'Display Name',
+        type: 'text',
+        required: false,
+        admin: {
+          position: 'sidebar',
+          readOnly: true,
+        },
+        i18n: true, // <-- add i18n property
+      },
+    ],
+    admin: {
+      useAsTitle: 'displayName',
     },
-  ],
-  admin: {
-    useAsTitle: 'displayName',
-  },
-  hooks: {
-    beforeValidate: [generateDisplayName],
-  },
-};
-
-export default ICD10;
+    hooks: {
+      beforeValidate: [generateDisplayName],
+    },
+  };
+  
