9622d52d3db17b461b4a37b18ef3c8ab1cc04b7b
dominikmukrecki <64379788+dominikmukrecki@users.noreply.github.com>
2023-03-06 12:41:02 +0100

    Added before validate hook to generate display name for users and merged userFields into Users object

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -13,58 +13,6 @@ const generateDisplayName: CollectionBeforeValidateHook<UserData> = async ({ dat
   return data;
 };
 
-const userFields = [
-  {
-    name: 'name',
-    label: 'Name',
-    type: 'text',
-    required: true,
-  },
-  {
-    name: 'email',
-    label: 'Email',
-    type: 'email',
-    required: true,
-  },
-  {
-    name: 'roles',
-    label: 'Roles',
-    type: 'select',
-    options: [
-      {
-        label: 'Admin',
-        value: 'admin',
-      },
-      {
-        label: 'Doctor',
-        value: 'doctor',
-      },
-      {
-        label: 'Assistant',
-        value: 'assistant',
-      },
-    ],
-    hasMany: true,
-    defaultValue: ['assistant'],
-    required: true,
-    admin: {
-      position: 'sidebar',
-    },
-  },
-  {
-    name: 'displayName',
-    label: 'Display Name',
-    type: 'text',
-    required: true,
-    defaultValue: 'New User',
-    admin: {
-      position: 'sidebar',
-      readOnly: true,
-    },
-  },
-  // Add more fields as needed
-];
-
 const Users: CollectionConfig = {
   slug: 'users',
   auth: true,
@@ -74,7 +22,57 @@ const Users: CollectionConfig = {
   access: {
     read: () => true,
   },
-  fields: userFields,
+  fields: [
+    {
+      name: 'name',
+      label: 'Name',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'email',
+      label: 'Email',
+      type: 'email',
+      required: true,
+    },
+    {
+      name: 'roles',
+      label: 'Roles',
+      type: 'select',
+      options: [
+        {
+          label: 'Admin',
+          value: 'admin',
+        },
+        {
+          label: 'Doctor',
+          value: 'doctor',
+        },
+        {
+          label: 'Assistant',
+          value: 'assistant',
+        },
+      ],
+      hasMany: true,
+      defaultValue: ['assistant'],
+      required: true,
+      admin: {
+        position: 'sidebar',
+      },
+    },
+    {
+      name: 'displayName',
+      label: 'Display Name',
+      type: 'text',
+      required: true,
+      defaultValue: 'New User',
+      admin: {
+        position: 'sidebar',
+        readOnly: true,
+      },
+    },
+    // Add more fields as needed
+  ],
   hooks: {
     beforeValidate: [generateDisplayName],
   },
