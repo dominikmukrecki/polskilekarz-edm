0de7edfd6592348e5ffcef9e8b52ba2d9943eb10
dominikmukrecki <dominik@mukrecki.pl>
2023-03-06 21:29:09 +0100

    Add generateDisplayName hook to generate a display name field and use it as admin.useAsTitle for each collection

---
+++ src/hooks/generateDisplayName.ts
@@ -0,0 +1,36 @@
+import { CollectionBeforeChangeHook, Context } from 'payload/types';
+
+type RecordData = {
+  [key: string]: any;
+};
+
+type GenerateDisplayNameArgs = {
+  fieldSlugs: string[];
+  separator: string;
+  displayNameField?: string;
+  useAsTitle?: string;
+};
+
+const generateDisplayName = ({ fieldSlugs, separator = ', ', displayNameField = 'displayName', useAsTitle }: GenerateDisplayNameArgs) => {
+  return async ({ data }: Context<RecordData>): Promise<RecordData> => {
+    let displayName = '';
+    for (let i = 0; i < fieldSlugs.length; i++) {
+      const slug = fieldSlugs[i];
+      const value = data[slug];
+      if (value) {
+        const label = await payload.fields.findOne({ where: { slug } }).then((field) => field?.admin?.label || slug);
+        displayName += `${label}: ${value}${i === fieldSlugs.length - 1 ? '' : separator}`;
+      }
+    }
+    data[displayNameField] = displayName;
+    if (useAsTitle) {
+      data.admin = {
+        ...data.admin,
+        useAsTitle,
+      };
+    }
+    return data;
+  };
+};
+
+export default generateDisplayName;
