6453644fd71c3c46d8f085851264b1a8673ffbb2
dominikmukrecki <64379788+dominikmukrecki@users.noreply.github.com>
2023-03-08 10:52:12 +0100

    test: Add test case for defaultDisplayName option in addDisplayName plugin

--- src/plugins/addDisplayName.ts
+++ src/plugins/addDisplayName.ts
@@ -1,14 +1,18 @@
 import { Config, Plugin } from 'payload/config';
 
+type Collection = {
+  [key: string]: any;
+};
+
 const addDisplayName: Plugin = (incomingConfig: Config): Config => {
   // Spread the existing config
   const config: Config = {
     ...incomingConfig,
     collections: incomingConfig.collections.map((collection) => {
-
       // Spread each item that we are modifying,
       // and add our new field - complete with
       // hooks and proper admin UI config
+      const defaultDisplayName = `New ${collection.label}`;
       return {
         ...collection,
         fields: [
@@ -17,7 +21,7 @@ const addDisplayName: Plugin = (incomingConfig: Config): Config => {
             name: 'displayName',
             label: 'Display Name',
             type: 'text',
-            required: true,
+            defaultValue: defaultDisplayName,
             admin: {
               position: 'sidebar',
               readOnly: true,
