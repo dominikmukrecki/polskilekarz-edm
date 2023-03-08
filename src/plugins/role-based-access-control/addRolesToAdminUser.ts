b7e2e94346729937a5e8c89587f6c710c996fc08
dominikmukrecki <dominik@mukrecki.pl>
2023-03-08 07:10:17 +0100

    Add roles field to admin user collection with global roles collection in Payload app

--- src/plugins/role-based-access-control/addRolesToAdminUser.ts
+++ src/plugins/role-based-access-control/addRolesToAdminUser.ts
@@ -1,37 +1,64 @@
 import { Config, Plugin } from 'payload/config';
 
 const addRolesToAdminUser: Plugin = async (incomingConfig: Config): Promise<Config> => {
-  // Get the slug of the default admin collection from the config
-  const adminCollectionSlug = incomingConfig.admin?.collection?.slug;
+  // Get the slug of the authentication collection from the config
+  const authCollectionSlug = incomingConfig.admin?.user;
 
-  if (!adminCollectionSlug) {
-    throw new Error('No admin collection specified in the config');
+  if (!authCollectionSlug) {
+    throw new Error('No authentication collection specified in the config');
   }
 
-  // Find the default admin collection
-  const adminCollectionIndex = incomingConfig.collections.findIndex(
-    collection => collection.slug === adminCollectionSlug
+  // Find the authentication collection
+  const authCollection = incomingConfig.collections.find(
+    collection => collection.slug === authCollectionSlug
   );
 
-  if (adminCollectionIndex === -1) {
-    throw new Error(`The '${adminCollectionSlug}' collection was not found`);
+  if (!authCollection) {
+    throw new Error(`The '${authCollectionSlug}' collection was not found`);
   }
 
-  // Update the collection to add the 'roles' field with the retrieved roles
-  const adminCollection = incomingConfig.collections[adminCollectionIndex];
-  adminCollection.fields.push({
+  // Find the global roles collection
+  const rolesCollection = incomingConfig.collections.find(
+    collection => collection.slug === 'roles'
+  );
+
+  if (!rolesCollection) {
+    throw new Error('The roles collection was not found');
+  }
+
+  // Update the admin user collection to add the 'roles' field with the retrieved roles
+  const adminUserCollection = incomingConfig.collections.find(
+    collection => collection.slug === 'admin.user'
+  );
+
+  if (!adminUserCollection) {
+    throw new Error('The admin.user collection was not found');
+  }
+
+  const rolesField = {
     name: 'roles',
     type: 'relationship',
-    relationTo: 'roles',
+    relationTo: rolesCollection.slug,
     hasMany: true,
     admin: {
       position: 'sidebar',
     },
-  });
+  };
+
+  if (adminUserCollection.fields) {
+    adminUserCollection.fields.push(rolesField);
+  } else {
+    adminUserCollection.fields = [rolesField];
+  }
 
   // Update the collection in the config
-  const collections = [...incomingConfig.collections];
-  collections[adminCollectionIndex] = adminCollection;
+  const collections = incomingConfig.collections.map(collection => {
+    if (collection.slug === 'admin.user') {
+      return adminUserCollection;
+    }
+
+    return collection;
+  });
 
   return {
     ...incomingConfig,
