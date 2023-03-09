6eaed574f8ce554f52fbe98c0c1a2416890374fc
dominikmukrecki <64379788+dominikmukrecki@users.noreply.github.com>
2023-03-09 12:35:50 +0100

    Refactor code to improve readability and remove unused plugin

--- src/plugins/metadataPlugin/index.ts
+++ src/plugins/metadataPlugin/index.ts
@@ -1,10 +1,20 @@
 import { Config, Plugin } from 'payload/config';
-import addLastModified from './addLastModified';
 import addCreatedBy from './addCreatedBy';
 import addIsArchived from './addIsArchived';
+import addLastModified from './addLastModified';
 
-const metadataPlugin: Plugin = (incomingConfig: Config): Config => {
-  return addCreatedBy(addIsArchived(addLastModified(incomingConfig)));
+const composePlugins = (...plugins: Plugin[]): Plugin => {
+  return (config: Config) => {
+    return plugins.reduce((acc, plugin) => {
+      return plugin(acc);
+    }, config);
+  };
 };
+
+const metadataPlugin = composePlugins(
+  addIsArchived,
+  addCreatedBy,
+  addLastModified,
+);
 
 export default metadataPlugin;
