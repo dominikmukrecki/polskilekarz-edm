5de3ab46c7b47699c7ff6253d4e3ccf232c7aba7
dominikmukrecki <64379788+dominikmukrecki@users.noreply.github.com>
2023-03-09 12:14:36 +0100

    Combine plugins into metadataPlugin in index.ts

--- src/plugins/metadataPlugin/index.ts
+++ src/plugins/metadataPlugin/index.ts
@@ -1,11 +1,10 @@
-import { addLastModified } from './addLastModified';
-import { addCreatedBy } from './addCreatedBy';
-import { addIsArchived } from './addIsArchived';
+import { Config, Plugin } from 'payload/config';
+import addLastModified from './addLastModified';
+import addCreatedBy from './addCreatedBy';
+import addIsArchived from './addIsArchived';
 
-const metadataPlugin = () => [
-  addLastModified(),
-  addCreatedBy(),
-  addIsArchived(),
-];
+const metadataPlugin: Plugin = (incomingConfig: Config): Config => {
+  return addCreatedBy(addIsArchived(addLastModified(incomingConfig)));
+};
 
 export default metadataPlugin;
