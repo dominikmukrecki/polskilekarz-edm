1df6b0455319c880e3a77a21b8fc40dfc26ea349
dominikmukrecki <64379788+dominikmukrecki@users.noreply.github.com>
2023-03-09 17:09:44 +0100

    Merge admin group into PersonalData using reduce function

--- src/collections/personalData/index.ts
+++ src/collections/personalData/index.ts
@@ -1,23 +1,21 @@
-import Contacts from './Contacts';
-import Emails from './Emails';
-import PhoneNumbers from './PhoneNumbers';
-import Addresses from './Addresses';
-
-const PersonalData = [
-  Contacts,
-  Emails,
-  PhoneNumbers,
-  Addresses,
-];
-
 const adminGroup = 'Personal Data';
 
-PersonalData.forEach(collection => {
-  if (collection.admin) {
-    collection.admin.group = adminGroup;
-  } else {
-    collection.admin = { group: adminGroup };
-  }
-});
+const PersonalData = [
+  ...Contacts,
+  ...Emails,
+  ...PhoneNumbers,
+  ...Addresses,
+].reduce((acc, collection) => {
+  return [
+    ...acc,
+    {
+      ...collection,
+      admin: {
+        ...(collection.admin || {}),
+        group: adminGroup,
+      },
+    },
+  ];
+}, []);
 
 export default PersonalData;
