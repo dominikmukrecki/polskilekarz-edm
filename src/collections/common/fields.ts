de8f11afd7dc966eae23686d1cce69e101ccfd13
dominikmukrecki <dominik@mukrecki.pl>
2023-03-11 19:45:32 +0100

    Add phone number field to phone numbers collection configuration.

--- src/collections/common/fields.ts
+++ src/collections/common/fields.ts
@@ -30,13 +30,12 @@ export const addressList: Field = {
   required: true,
 };
 
-export const addressList: Field = {
-  name: "addressList",
-  type: "relationship",
-  relationTo: "addresses",
-  hasMany: true,
-  required: true,
-};
+export const addressField: Field = {
+    name: 'address',
+    type: 'text',
+    required: true,
+    unique: true,
+  };
 
 export const emailField: Field = {
   name: "email",
@@ -44,3 +43,17 @@ export const emailField: Field = {
   required: true,
   unique: true,
 };
+
+export const phoneNumberField: Field = {
+    name: 'number',
+    type: 'text',
+    required: true,
+    unique: true,
+    validate: (value) => {
+      const regex = /^\+(?:[0-9] ?){6,14}[0-9]$/;
+      if (value && !regex.test(value)) {
+        return 'Invalid phone number format';
+      }
+      return true;
+    },
+  };  
